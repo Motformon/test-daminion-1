1df90384dc0c9e815b9cff646755da47a7334bf3
motformon <motformon@gmail.com>
2019-04-14 11:22:47 +0300

    add typescript and create structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+				
+			</React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/IssuingTickets/IssuingTickets.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import "./IssuingTickets.scss";
+
+class IssuingTickets extends Component {
+
+	state = {
+
+	}
+
+  render() {
+    return (
+			<div className="IssuingTickets">
+
+			</div>
+    );
+  }
+}
+
+
+
+export default IssuingTickets;
+
